d73e02b672dbf7ef0667d00703f44cc7bd40ce14
zjut_crawler <1979837154@qq.com>
2025-03-03 21:05:17 +0800

    class

--- arxiv_questes/arxiv_html.py
+++ arxiv_questes/arxiv_html.py
@@ -13,10 +13,15 @@ BASE_URL = 'https://arxiv.org/search/'
 
 
 class ArxivCrawler:
-    def __init__(self, query):
+    def __init__(self, query,div):
         self.query = query
-        # 请求头，用于模拟浏览器行为
-        self.headers = {
+        # 请求头，用于模拟浏览器行为,根据不同的操作系统设置不同的请求头
+        if div=='linux':
+            self.headers = {
+                'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36 Edg/132.0.0.0"
+            }
+        else:
+            self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36 Edg/133.0.0.0'}
 
     # 根据对应的url获取页面html内容
@@ -43,7 +48,7 @@ class ArxivCrawler:
 
     
 
-    # 通过使用
+    # 通过使用CSS选择器定位目标元素并获取文本
     def parse_index(self, html):
         # 获取总页数以及每页的论文数量
         doc = pq(html)
--- arxiv_questes/html_tempalte.py
+++ arxiv_questes/html_tempalte.py
@@ -5,11 +5,18 @@ import logging
 import requests
 from arxiv_html import ArxivCrawler
 
+real_html = """
+<h1 class="title is-clearfix">
+    Showing 1-50 of 1158 results for all: <span class="mathjax">Vehicle+trajectory+prediction</span>
+</h1>
+"""
+
 class CustomArxivCrawler(ArxivCrawler):
-    def __init__(self, query, html_content):
-        super().__init__(query)
-        self.html = html_content
-        self.base_url = "https://arxiv.org/search/?query=Vehicle%2Btrajectory%2Bprediction&searchtype=all&source=header"
+    def __init__(self, query):
+        # 调用父类的初始化方法
+        super().__init__(query,div='linux')
+        # 通过父类的方法来获取对应的HTML方法
+        self.html = self.scrape_index()
 
     def get_total_pages(self):
         # 使用 pyquery 解析 HTML
@@ -17,7 +24,7 @@ class CustomArxivCrawler(ArxivCrawler):
         # 使用 CSS 选择器定位目标元素并获取文本
         result_text = doc('.title.is-clearfix').text()
         # 提取总页数
-        total_pages_match = re.search(r'Showing (\d+)-(\d+) of (\d[\d,]*)\s*results', result_text)
+        total_pages_match = re.search(r'Showing (\d+)[–-](\d+) of (\d[\d,]*)\s*results', result_text)
         if total_pages_match:
             total_pages_str = total_pages_match.group(3).replace(',', '')
             # 每页的论文数量
@@ -28,9 +35,11 @@ class CustomArxivCrawler(ArxivCrawler):
             print(f"总页数为: {pages_num}")
             return pages_num, int(end_num_str)
         else:
+            print(result_text)
             print("未找到总页数信息。")
             return None, None
 
+    # 爬取所有页面
     def crawl_all_pages(self):
         pages_num, end_num = self.get_total_pages()
         if pages_num is not None:
@@ -43,13 +52,7 @@ class CustomArxivCrawler(ArxivCrawler):
                 print(self.scrape_page(full_url))
 
 
-# 使用示例
-if __name__ == "__main__":
-    # 假设这是实际获取到的 HTML 内容
-    real_html = """
-    <h1 class="title is-clearfix">
-        Showing 1-50 of 1158 results for all: <span class="mathjax">Vehicle+trajectory+prediction</span>
-    </h1>
-    """
-    crawler = CustomArxivCrawler('Vehicle+trajectory+prediction', real_html)
-    crawler.crawl_all_pages()
+if __name__ == '__main__':
+    query = 'Vehicle+trajectory+prediction'
+    crawler = CustomArxivCrawler(query)
+    crawler.get_total_pages()
